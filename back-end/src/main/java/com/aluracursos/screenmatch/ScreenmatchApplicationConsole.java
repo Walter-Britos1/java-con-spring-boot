9db285e965b40728250b6b51fb9b09131ae3029f
Walter-Britos1 <walterbritos418@gmail.com>
2024-12-28 22:50:24 -0300

    adding controllers

--- back-end/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplicationConsole.java
+++ back-end/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplicationConsole.java
@@ -1,25 +1,25 @@
-package com.aluracursos.screenmatch;
-
-import com.aluracursos.screenmatch.Main.Main;
-import com.aluracursos.screenmatch.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class ScreenmatchApplicationConsole implements CommandLineRunner {
-    @Autowired
-    private SerieRepository repository;
-
-	public static void main(String[] args) {SpringApplication.run(ScreenmatchApplicationConsole.class, args);}
-
-    @Override
-    public void run(String... args) throws Exception {
-        Main main = new Main(repository);
-        main.showMenu();
-
-//        ExampleStream exampleStream = new ExampleStream();
-//        exampleStream.showNames();
-    }
-}
+//package com.aluracursos.screenmatch;
+//
+//import com.aluracursos.screenmatch.Main.Main;
+//import com.aluracursos.screenmatch.repository.SerieRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class ScreenmatchApplicationConsole implements CommandLineRunner {
+//    @Autowired
+//    private SerieRepository repository;
+//
+//	public static void main(String[] args) {SpringApplication.run(ScreenmatchApplicationConsole.class, args);}
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        Main main = new Main(repository);
+//        main.showMenu();
+//
+////        ExampleStream exampleStream = new ExampleStream();
+////        exampleStream.showNames();
+//    }
+//}
